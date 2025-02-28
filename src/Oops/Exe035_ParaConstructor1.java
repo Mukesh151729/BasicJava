87291850b4283a620c8dac1e5d68fa4fbb0bd9da
Mukesh Dhage <dhage.mukesh1729@gmail.com>
2025-02-28 17:23:09 +0530

    new code

---
+++ src/Oops/Exe035_ParaConstructor1.java
@@ -0,0 +1,42 @@
+package Oops;
+
+public class Exe035_ParaConstructor1 {
+    public static void main(String[] args) {
+
+        mobile iphone=new mobile("iphone","2008");
+        mobile oppo =new mobile("oppo","2020");
+
+        System.out.println(iphone.mobile_name+" "+ iphone.year);
+        System.out.println(oppo.mobile_name+" "+ oppo.year);
+
+        iphone.display();
+
+        String modelName=oppo.printModelName();
+        System.out.println(modelName);
+
+    }
+
+
+
+}
+
+class mobile{
+    String mobile_name;
+    String year;
+
+    public mobile(String mobile_name_c, String year_c) {
+        this.mobile_name = mobile_name_c;
+        this.year = year_c;
+    }
+
+    void display()
+    {
+        System.out.println(mobile_name+" "+year);
+    }
+
+    String printModelName()
+    {
+        return mobile_name;
+    }
+
+}
